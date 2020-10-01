9cfe0ba96eb1df07cf45ebcae93e97aaae51dacf
Michelle Yi <m7890yi@gmail.com>
2020-10-01 19:56:39 -0400

    player clicks start

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,10 @@
-this is gibberish
+window.addEventListener("DOMContentLoaded", (event) => {
+  const playerXClick = function (box) {
+    let clickedBox = document.getElementById(box);
+    
+  };
+  document.body.addEventListener("click", (event) => {
+    // console.log(event.target.id);
+    playerXClick(event.target.id);
+  });
+});
