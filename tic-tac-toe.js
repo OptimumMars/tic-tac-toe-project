4c1fc656352b6b30dabfc97a96abe329a8835df3
Gabriel <gabriel.gutz@live.com>
2020-10-02 12:20:56 -0400

    Added clicks and skeleton for turns of each player

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,34 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const playerXClick = function (box) {
     let clickedBox = document.getElementById(box);
-    
+
+    let xImage = document.createElement("img");
+    xImage.setAttribute("src", "player-x.svg");
   };
+
+  const playOClick = function(box){
+      let clickedBox = document.getElementById(box);
+
+      let oImage = document.createElement("img");
+      oImage.setAttribute("src", "player-o.svg")
+  }
+
+  let i = 1;
+
+  const clickFunction = function(){
+
+    if(i % 2 !== 0){
+        console.log("x click");
+        console.log(i);
+        i++
+    }else{
+        console.log("o click");
+        i++;
+    }
+  }
   document.body.addEventListener("click", (event) => {
-    // console.log(event.target.id);
+    clickFunction();
+
     playerXClick(event.target.id);
   });
 });
