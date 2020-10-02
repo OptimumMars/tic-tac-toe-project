c5de22e5df32a3ef7a901893ea807b51abc31e5a
Gabriel <gabriel.gutz@live.com>
2020-10-02 15:45:36 -0400

    Added x and o display functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,33 +2,38 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const playerXClick = function (box) {
     let clickedBox = document.getElementById(box);
 
-    let xImage = document.createElement("img");
-    xImage.setAttribute("src", "player-x.svg");
+    // let xImage = document.createElement("img");
+    // xImage.setAttribute("src", "player-x.svg");
+
+    clickedBox.innerHTML = "<img src = './player-x.svg'>";
   };
 
-  const playOClick = function(box){
+  const playerOClick = function(box){
       let clickedBox = document.getElementById(box);
 
       let oImage = document.createElement("img");
-      oImage.setAttribute("src", "player-o.svg")
+      oImage.setAttribute("src", "player-o.svg");
+
+      clickedBox.appendChild(oImage);
   }
 
   let i = 1;
 
-  const clickFunction = function(){
+  const clickFunction = function(boxID){
 
     if(i % 2 !== 0){
         console.log("x click");
         console.log(i);
+        playerXClick(boxID);
         i++
     }else{
         console.log("o click");
+        playerOClick(boxID);
         i++;
     }
   }
-  document.body.addEventListener("click", (event) => {
-    clickFunction();
 
-    playerXClick(event.target.id);
+  document.body.addEventListener("click", (event) => {
+    clickFunction(event.target.id);
   });
 });
